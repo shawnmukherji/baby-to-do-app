13e5f9ba8d9f27657ca8a2015d4d17cc5ab64ecd
Shawn Mukherji <Shawn@Shawns-Mac.local>
2017-10-12 17:07:06 -0500

    foundation, login.js

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -11,3 +11,27 @@ var Signup = require('signup');
 var Password = require('password');
 
 var token = window.localStorage.getItem('token');
+
+if (token) {
+    axios.defaults.headers.common['x-auth'] = token;
+}
+
+var requireLogin = (nextState, replace, next) => {
+    if(token) {
+        replace('/todos');
+    }
+    next();
+};
+
+var redirectIfLogged = (nextState, replace, next) => {
+    if (token) {
+        replace('/todos');
+    }
+    next();
+};
+
+//Initialize foundation.js to start the first page
+$(document).foundation();
+
+//App css-sass
+require('style!')
